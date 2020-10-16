418dfe73e2902bb5bbbd3288dd3ed2b89f6db8bd
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-10-16 06:55:20 -0300

    Inserindo mais funções.

---
+++ plotando/plot_v_irracional_1.py
@@ -0,0 +1,32 @@
+"""
+Vamos plotar uma função irracional
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# definição da função:
+def f(x):
+    if x > 0:
+        return 1 / np.sqrt(x)
+
+
+# Vamos criar valores para x:
+x = range(1, 100)
+
+
+# Vamos obter os valores de y:
+y = [f(i) for i in x]
+
+
+# Vamos construir o gráfico:
+plt.xlabel('eixo X')
+plt.ylabel('eixo Y')
+plt.grid()
+
+
+# Vamos plotar a função:
+plt.plot(x, y, color='blue')
+
+plt.show()
