1f7503a695231c3f39922ab925ad8de90b615804
Suenildo Pereira de Sousa <suenildo_sousa@outlook.com>
2020-10-22 06:44:36 -0300

    Iniciando o pacote: Cálculo Diferencial.

---
+++ calculo_diferencial/calculo_v0.py
@@ -0,0 +1,27 @@
+"""
+Vamos começar esse estudo com uma função simples.
+"""
+from matplotlib import pyplot as plt
+
+
+# Vamos definir nossa função:
+def f(x):
+    return x ** 2 + x
+
+
+# Vamos criar valores para x:
+x = list(range(0, 20))
+
+# Vamos atribuir os valores de y:
+y = [f(i) for i in x]
+
+# Construção do gráfico:
+plt.xlabel('x')
+plt.ylabel('f(x)')
+
+# Plotagem do gráfico:
+plt.plot(x, y, color='black')
+
+# Chamada do gáfico:
+plt.show()
+
